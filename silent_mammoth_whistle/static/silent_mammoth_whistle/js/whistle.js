7ab814e30847b6cb5f7a863e9776d1b1cf2bc601
jbaack <jbaack@accelerantsys.com>
2025-03-11 07:45:05 -0700

    Update whistle.js - don't set cookie

--- silent_mammoth_whistle/static/silent_mammoth_whistle/js/whistle.js
+++ silent_mammoth_whistle/static/silent_mammoth_whistle/js/whistle.js
@@ -9,7 +9,7 @@ function whistle(...args) {
 	fetch(_smwUrl, { method: 'POST', body: formData })
 }
 
-document.cookie = "viewport_dimensions=" + window.outerWidth + 'x' + window.outerHeight + '; path=/;';
+/* document.cookie = "viewport_dimensions=" + window.outerWidth + 'x' + window.outerHeight + '; path=/;';    DON'T SET COOKIE */
 
 /***
  * This helps ignore bots when counting whistles. Bots don't seem to stick around long enough to execute a delayed JavaScript call so we make an extra web request after a 3 second delay. We then filter these requests out on the Django side so they're not counted as whistles in a session. SessionStorage is used so the call only happens once per session.
